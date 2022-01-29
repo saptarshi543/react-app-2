ed3d4645a90694b09480a5a23002085ffbde88b4
saptarshi chattopadhyay <saptarshichattopadhyay543@gmail.com>
2022-01-29 15:20:42 +0530

    changed buttons to pictures.working..

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -14,19 +14,28 @@ const ImageSlider=({images})=>{
             setIndex(nextIndex);
         }
     };
-    const buttonStyles={
-        width:"70px",
-        height:"20px"
+    
+    // const buttonStyles={
+    //     display:"hidden",
+    //     width:"100px",
+    //     height:"200px"
+    // };
+
+    const imgStyles={
+        width:"100px",
+        height:"70px"
     };
 
     return(
         images.length > 0 && (
             <div>
-                <button onClick={slideLeft}  style={buttonStyles}>{"Previous"}</button>
+                {/* <button onClick={slideLeft}  style={buttonStyles}> <img style={imgStyles} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbggreensource.org%2Fwp-content%2Fuploads%2F2018%2F06%2FPrevious-Button-Transparent-Background.png&f=1&nofb=1" alt="Previous"/> </button> */}
+                <img style={imgStyles} onClick={slideLeft} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fbggreensource.org%2Fwp-content%2Fuploads%2F2018%2F06%2FPrevious-Button-Transparent-Background.png&f=1&nofb=1" alt="Previous" />
                 <br></br>
                 <img src={images[index]} alt={index} />
                 <br></br>
-                <button onClick={slideRight} style={buttonStyles}>{"Next"}</button>
+                {/* <button onClick={slideRight} style={buttonStyles}> <img style={imgStyles} src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fclipartmag.com%2Fimages%2Fnext-button-clipart-9.png&f=1&nofb=1" alt="Next"/> </button> */}
+                <img style={imgStyles} onClick={slideRight} src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fclipartmag.com%2Fimages%2Fnext-button-clipart-9.png&f=1&nofb=1" alt="Next" />
             </div>
         )
     );
