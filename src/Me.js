9aee458743277308d7b827e54eed2d46f851d0d5
saptarshi chattopadhyay <saptarshichattopadhyay543@gmail.com>
2022-12-22 20:07:14 +0530

    modified about me

--- src/Me.js
+++ src/Me.js
@@ -1,14 +1,22 @@
 import React from "react";
 function Me(){
-    return(
-        <div>
-            <div className="text">
+const style_m={
+	backgroundColor: '#2995e7',
+	padding:"20px"
+};
+const text={
+	backgroundColor:"#f1c232"
+}
+	return(
+        <div style={style_m} className="text">
             <h4>my E-mail: saptarshichatterjee543@gmail.com</h4>
-        <a href='https://github.com/saptarshi543' target="_blank" rel="noreferrer" >my github account</a>
+        <a style={text} href='https://github.com/saptarshi543'>my github account</a>
         <br></br>
-         <a href="https://www.buymeacoffee.com/sap543" target="_blank" rel="noreferrer" >pleasse support me here.</a>
+        <p1>Wonder why this site does not have a nice domain name?
+            That's because i am too poor to afford a site name.
+            So <a style={text} href="https://www.buymeacoffee.com/sap543">pleasse support me here.</a>
+        </p1>
         </div>
-        </div>
-    );
+    )
 }
 export default Me;
