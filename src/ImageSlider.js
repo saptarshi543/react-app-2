24fd0bfe57bd71f127789c3b7053aae7c0fe89d1
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2023-02-05 18:41:49 +0530

    updated buttons

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -15,19 +15,16 @@ const ImageSlider=({images})=>{
         }
     };
 
-    const imgStyles={
-	    width:"100px",
-        height:"70px"
-    };
 
     return(
         images.length > 0 && (
             <div>
-                <img style={imgStyles} onClick={slideLeft} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F000%2F442%2F785%2Foriginal%2Fprevious-vector-icon.jpg&f=1&nofb=1" alt="Previous" />
+<button onClick={slideLeft}>Previous</button>
                 <br></br>
                 <img class="final_results" style={styleR} src={images[index]} alt={index} />
                 <br></br>
-                <img style={imgStyles} onClick={slideRight} src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fclipartmag.com%2Fimages%2Fnext-button-clipart-9.png&f=1&nofb=1" alt="Next" />
+<button onClick={slideRight}>Next</button>
+
             </div>
         )
     );
