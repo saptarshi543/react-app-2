664ac88dba0b252bf9b8185a075965b72bcf460c
Saptarshi Chattopadhyay <saptarshichattopadhyay543@gmail.com>
2023-02-06 18:44:01 +0530

    updated env variables

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ const App=()=>{
 
   const handleSubmit=(e)=>{
     e.preventDefault();
-	  const k=`${process.env.REACT_APP_API_KEY}`;
-    console.log("test -> "+k);
+//	  const k=`${process.env.REACT_APP_API_KEY}`;
+  //  console.log("test -> "+k);
     fetch(
-      `https://pixabay.com/api/?key=${k}&q=${query}`
+      `https://pixabay.com/api/?key=25327586-7719c389d6bb2cfb9d48e1347&q=${query}`
     )
       .then((response) => response.json())
       .then(({ hits }) => hits.map(({ webformatURL }) => webformatURL)) // use object destructuring to grab image urls from json response
